25fb2e7264dc5836fb81fe1184fe96082382409a
javelin <1154686922@qq.com>
2019-07-21 22:46:00 +0800

    获取token模块

--- token.go
+++ token.go
@@ -4,7 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net/http"
+	"time"
+)
+
+const (
+	TOKEN_ERROR_1 = "系统繁忙，此时请开发者稍候再试"
+	TOKEN_ERROR_2 = "AppSecret错误或者AppSecret不属于这个公众号，请开发者确认AppSecret的正确性"
+	TOKEN_ERROR_3 = "请确保grant_type字段值为client_credential"
+	TOKEN_ERROR_4 = "调用接口的IP地址不在白名单中，请在接口IP白名单中进行设置。（小程序及小游戏调用不要求IP地址在白名单内"
 )
 
 type AccessToken struct {
@@ -14,17 +21,68 @@ type AccessToken struct {
 	Errmsg      string `json:"errmsg"`
 }
 
-func NewToken() (AccessToken, err) {
+type Token struct {
+	At AccessToken
+}
+
+var token Token
+
+//token 定期清理时间 
+var TokenGcTime = 7200
+
+
+//刷新token
+func (t *Token)Reflash() error {
 
 	url := "https://api.weixin.qq.com/cgi-bin/token?"
 	url += "grant_type=client_credential&appid=" + WxAppId + "&secret=" + WxAppSecret
 
-	var at AccessToken
-
-	err := NewRequest(&at).Get(url)
+	err := NewRequest(&t.At).Get(url)
 	if err != nil {
-		return nil, err
+		return err 
 	}
-
-	return at, nil
+	return nil
 }
+
+//获取token
+func (t *Token) Get() (string, error ){
+	if t.AccessToken == (AccessToken{}) {
+		err := t.Reflash()
+		if err != nil {
+			return nil, err
+		}
+
+		switch t.At.Errcode{
+		case -1 :
+			return errors.New(TOKEN_ERROR_1)
+		case 40001:
+			return errors.New(TOKEN_ERROR_2)	
+		case 40002:
+			return 	errors.New(TOKEN_ERROR_3)
+		case 40164:	
+			return errors.New(TOKEN_ERROR_4)
+		default :
+			return t.At.AccessToken
+		}
+	} 
+
+	return t.At.AccessToken
+}
+
+//定期清空 时间间隔为 TokenGcTime
+func (t. Token) clear(){
+	duration := time.Duration(TokenGcTime) * time.Second
+	for {
+		<-time.After(duration)
+		if t.At != (AccessToken{}) {
+			t.At = AccessToken{} 
+		}
+	}		
+}
+
+
+func init(){
+	token = &Token{}
+	go token.clear()
+}
+
--- wechat.go
+++ wechat.go
@@ -6,10 +6,12 @@ var WxAppSecret string
 var MpAppid string
 var MpAppsecret string
 
+var token string
+
 func init() {
 	WxAppId = "wx75d0a800a00671a1"
 	WxAppSecret = "de3426ea07a05887a220c91232fcc9e7"
 	MpAppid = "wxd598f39ca93bd3d3"
-	MpAppsecret = "4cc8ce9de598c2fcfd3dce9d809cb585"			
+	MpAppsecret = "4cc8ce9de598c2fcfd3dce9d809cb585"	
 }
 
